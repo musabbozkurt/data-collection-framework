6b81c777fd78af48e687fc86b3b7fbafabf600d8
musabbozkurt <b_musab@hotmail.com>
2016-10-30 22:13:36 +0200

    Update work18-all timeline to txt.py

--- DM/work18-all timeline to txt.py
+++ DM/work18-all timeline to txt.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python
 # encoding: utf-8
-
-#import libraries
 import pymongo
 from pymongo import MongoClient
 import tweepy #https://github.com/tweepy/tweepy
 import json
 import glob
 
-#Twitter API credentials
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
+class AllVariableClass:
+        # Twitter API credentials
+        consumer_key = ""
+        consumer_secret = ""
+        access_key = ""
+        access_secret = ""
 
-#authorize twitter, initialize tweepy
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth)
+        # authorize twitter, initialize tweepy
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_key, access_secret)
+        api = tweepy.API(auth)
 
-#files direction
-filePathForOutputs = ""
-filePathForFollowersOutputs = ""
-ListOfUsernamefilepath = ''
-filepathformongo = ""
+        # files direction
+        filePathForOutputs = "give path for user timelines which are stored as txt file"
+        filePathForFollowersOutputs = "give a path followers txt file"
+        ListOfUsernamefilepath = 'give a txt file path which contains username list '
+        filepathformongo = "give a path which contains txt files waiting for to be added mongodb"
 
-#create mongodb database
-client = MongoClient()
-db = client.twitter3
-streamingdb='streamingdb'
+        # create mongodb database
+        client = MongoClient()
+        db = client.twitter3
+        streamingdb = 'streamingdb'
+
 
 # getting all tweets in users timeline according to their username.
-class TweetCollector:
+class TweetCollector():
         def get_all_tweets(screen_name):
             #Twitter only allows access to a users most recent 3240 tweets with this method
 
@@ -39,7 +39,7 @@ class TweetCollector:
             alltweets = []
 
             #make initial request for most recent tweets (200 is the maximum allowed count)
-            new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+            new_tweets = AllVariableClass.api.user_timeline(screen_name = screen_name,count=200)
 
             #save most recent tweets
             alltweets.extend(new_tweets)
@@ -48,11 +48,11 @@ class TweetCollector:
             oldest = alltweets[-1].id - 1
 
             #keep grabbing tweets until there are no tweets left to grab
-            while len(new_tweets) > 0:
+            while (len(new_tweets) > 0):
                 print ("getting tweets before %s" % (oldest))
 
                 #all subsiquent requests use the max_id param to prevent duplicates
-                new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+                new_tweets = AllVariableClass.api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
 
                 #save most recent tweets
                 alltweets.extend(new_tweets)
@@ -61,16 +61,17 @@ class TweetCollector:
                 oldest = alltweets[-1].id - 1
 
                 print ("...%s tweets downloaded so far" % (len(alltweets)))
-
+                # control api to do not exceed the 1000 limits                
+                if (len(alltweets)) == 1000:
+                    break
             #write tweets to the txt files
             for tweet in alltweets:
-                f = open(filePathForOutputs + screen_name + ".txt", "a")
+                f = open(AllVariableClass.filePathForOutputs + screen_name + ".txt", "a")
                 f.write(json.dumps(tweet._json) + "\n")
             print(screen_name+"'s tweets added to file")
             pass
-
 # this class write all txt files which are in the filepath direction into mongodb database
-class MongoWriter:
+class MongoWriter():
     def writetoMongo(filepath):
          # and inside that DB, a collection called "files"
          filenames = glob.glob(filepath)
@@ -78,7 +79,7 @@ class MongoWriter:
          for filename in filenames:
            with open(filename) as f:
              for line in f:
-                 db.timeline.insert_one(json.loads(line))
+                 AllVariableClass.db.timeline.insert_one(json.loads(line))
 
 # stremaming class collect tweets according to keywords which are in the array
 class CustomStreamListener(tweepy.StreamListener):
@@ -93,7 +94,7 @@ class CustomStreamListener(tweepy.StreamListener):
         self.db.tweets.insert(json.loads(tweet))
 
     def on_sapi(stwets):
-        sapi = tweepy.streaming.Stream(auth, CustomStreamListener(api))
+        sapi = tweepy.streaming.Stream(AllVariableClass.auth, CustomStreamListener(AllVariableClass.api),q='geocode:"37.781157,-122.398720,1mi" since:2016-10-30 until:2016-10-31 include:retweets')
         sapi.filter(track=stwets)
 
     def on_error(self, status_code):
@@ -101,30 +102,29 @@ class CustomStreamListener(tweepy.StreamListener):
 
     def on_timeout(self):
         return True # Don't kill the stream
-
-#Collection all followers on twitter according to useername
+#
 class FollowerCollector():
     def write_on_file(self,screenname):
-        for follower in api.followers_ids(screenname):
+        for follower in AllVariableClass.api.followers_ids(screenname):
             try:
-                with open(filePathForFollowersOutputs + screenname + ".txt", "a") as f:
-                    f.write(api.get_user(follower).screen_name + ' \n')
+                with open(AllVariableClass.filePathForFollowersOutputs + screenname + ".txt", "a") as f:
+                    f.write(AllVariableClass.api.get_user(follower).screen_name + ' \n')
             except:
                 print(screenname+str(IOError))
                 pass
 
 if __name__ == '__main__':
-    #pass in the username of the account you want to download
+    # pass in the username of the account you want to download
 
-     with open(ListOfUsernamefilepath,'r') as f:
+     with open(AllVariableClass.ListOfUsernamefilepath,'r') as f:
         try:
             for line in f:
                   for word in line.split():
                      TweetCollector.get_all_tweets(word)
                      FollowerCollector().write_on_file(word)
-        except:
-            print(str(IOError))
-            pass
 
-     MongoWriter.writetoMongo(filepathformongo)
-     CustomStreamListener.on_sapi(['ygs','indian'])
+            MongoWriter.writetoMongo(AllVariableClass.filepathformongo)
+            CustomStreamListener.on_sapi(['ygs', 'indian'])
+        except:
+            # print(str(IOError))
+            pass
