07291aa36aa6827ebdd8e7bfbe37fd8623fffbed
musab.bozkurt <musab.bozkurt@ozan.com>
2024-10-04 05:57:46 +0300

    refactor-code - fix connection config

--- src/dbconnect.php
+++ src/dbconnect.php
@@ -1,4 +1,3 @@
 <?php
-//connect to mysql database
-$con = new mysqli("localhost", "user", "user", "test_db") or die("Error " . mysqli_error($con));
-?>
+// connect to mysql database
+$con = new mysqli("db", "root", "root", "test_db") or die("Error " . mysqli_error($con));
